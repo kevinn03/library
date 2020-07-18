1a8fec7b51c0380c3de9d2cd274c8eb0afa1de72
Kevin <kevin03.nguyen@gmail.com>
2020-07-17 20:46:40 -0400

    added status and delete button, added the render function which loads the library's books

--- lib.js
+++ lib.js
@@ -1,4 +1,8 @@
 let myLibrary = [];
+let prop = ["title", "author", "pages", "read"]
+
+
+
 
 
 
@@ -21,3 +25,48 @@ function addBookToLibrary(){
     myLibrary.push(toAdd);
 }
 
+function render(){
+    
+    let table = document.querySelector(".table");
+    
+    for (let i = 0; i < myLibrary.length; i++) {
+        
+        let row = document.createElement("tr")
+        row.classList.add(`row${i}`)
+        
+        for (let j = 0; j < 4; j++) {
+            
+            let data = document.createElement("td");
+            data.textContent = myLibrary[i][prop[j]];
+            data.classList.add(`data${j}`)
+            row.appendChild(data);
+        }
+        let readtd = document.createElement("td");
+        let buttd = document.createElement("button");
+        buttd.textContent = "Status";
+        buttd.classList.add(`status${i}`);
+        buttd.classList.add("status");
+        readtd.appendChild(buttd);
+        
+        let deltd = document.createElement("td");
+        let butdel = document.createElement("button");
+        butdel.textContent = "X";
+        butdel.classList.add(`del${i}`);
+        butdel.classList.add("delete");
+        deltd.appendChild(butdel);
+
+        row.appendChild(readtd);
+        row.appendChild(deltd);
+        table.appendChild(row);
+        
+    }
+}
+
+//test
+let Harry = new Book("Harry Potter", "JK", "400", "read");
+myLibrary.push(Harry);
+
+let Lotr = new Book("Lotr", "Tolken", "400", "read");
+myLibrary.push(Lotr);
+
+render();
